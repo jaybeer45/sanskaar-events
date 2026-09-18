1ee48766fab4bda8b14dd9200d866e9d9a3a65f4
jaybeer <kandwaljaybeer@gmail.com>
2026-09-18 14:28:28 +0530

    fix mailer

--- sanskaar-backend/src/config/mailer.js
+++ sanskaar-backend/src/config/mailer.js
@@ -8,6 +8,9 @@ const nodemailer = require('nodemailer');
 //    on your laptop — production reads from Render's env settings)
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host:"smtp.gmail.com",
+  port:587,
+  secure:false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
